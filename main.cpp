7d8e3898b67d429a36a0b67495a516e62a83edbf
spoooktune <asriprasana5@gmail.com>
2025-11-30 18:06:16 -0800

    Added menu output, began coding functionality

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     IntBinaryTree tree;
     ifstream codes;
+    int choice = 0;
     codes.open("codes.txt");
     if (codes.good()){
         string tempC;
@@ -14,6 +15,21 @@ int main() {
         }
     }
     codes.close();
-    tree.displayInOrder();
+    //tree.displayInOrder();
+    while (choice != 4){
+        cout << "BST Menu" << endl;
+        cout << "[1] Add Code" << endl;
+        cout << "[2] Delete Code" << endl;
+        cout << "[3] Search Tree" << endl;
+        cout << "[4] Quit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+                string tempC;
+                cout << "Enter code to add: ";
+        }
+    }
     return 0;
 }
