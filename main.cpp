eb0c6de53ac34c52cc5f3fcbc801391b9847ba2a
spoooktune <asriprasana5@gmail.com>
2025-11-30 17:39:33 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main() {
-    
-
+    IntBinaryTree tree;
+    tree.insertNode(4);
+    tree.insertNode(5);
+    tree.insertNode(3);
+    tree.displayInOrder();
+    cout << endl;
+    tree.displayPreOrder();
+    cout << endl;
+    tree.displayPostOrder();
     return 0;
 }
